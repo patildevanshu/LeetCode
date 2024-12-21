86940a13a78523ac125a0087b09b287203c854f3
DEVANSHU PATIL <152208944+patildevanshu@users.noreply.github.com>
2024-12-21 14:21:50 +0530

    Time: 1 ms (99.39%), Space: 57 MB (49.34%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,24 +1,20 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        int n = nums.length;
-
-        // System.out.println(n);
-        int max=Integer.MIN_VALUE;
-        int negativeInt = Integer.MIN_VALUE;
-        int sum =0;
-
-        for(int i=0 ; i<n ;i++){
-            negativeInt = Math.max(negativeInt , nums[i]);
-            sum =(nums[i] + sum) <= 0 ? 0 : sum + nums[i];
-            if(max<sum){
-                max=sum;
+        int maxSum = Integer.MIN_VALUE;
+        int currentSum = 0;
+        
+        for (int i = 0; i < nums.length; i++) {
+            currentSum += nums[i];
+            
+            if (currentSum > maxSum) {
+                maxSum = currentSum;
             }
-            if(max==0){
-                max=negativeInt;
+            
+            if (currentSum < 0) {
+                currentSum = 0;
             }
         }
-        // System.out.println(max);
-        return max;
-
+        
+        return maxSum;
     }
 }
