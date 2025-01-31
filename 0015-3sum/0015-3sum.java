581a55b58fbf03f179e7368fbd0d3dbcd52d2e7c
DEVANSHU PATIL <152208944+patildevanshu@users.noreply.github.com>
2025-01-31 09:06:18 +0530

    Time: 828 ms (16.43%), Space: 52.4 MB (28.45%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,67 +1,26 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        
-    List<List<Integer>> result = new ArrayList<>();
-
-    Arrays.sort(nums);
-
-    for(int i=0;i<nums.length-2;i++)
-    {
-       //skip duplicates
-       if(i>0 && nums[i]==nums[i-1])
-       {
-        continue;
-       }
-
-       int left = i+1;
-       int right = nums.length-1;
-
-
-       while(left<right)
-       {
-          int sum = nums[i] + nums[left] + nums[right];
-
-          if(sum==0)
-          {
-            result.add(Arrays.asList(nums[i],nums[left],nums[right]));
-
-            while(left<right && nums[left]==nums[left+1])
-            {
-                left++;
+        int target = 0;
+        Arrays.sort(nums);
+        Set<List<Integer>> s = new HashSet<>();
+        List<List<Integer>> output = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++){
+            int j = i + 1;
+            int k = nums.length - 1;
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum == target) {
+                    s.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    j++;
+                    k--;
+                } else if (sum < target) {
+                    j++;
+                } else {
+                    k--;
+                }
             }
-         
-           while(left<right && nums[right]==nums[right-1])
-           {
-            right--;
-           }
-
-           left++;
-           right--;
-
-
-          }
-
-          else if(sum<0)
-          {
-            left++;
-          }
-
-          else{
-            right--;
-          }
-
-
-
-       }
-
-
-
-
-
-
-    }
-  return result;
-
-
+        }
+        output.addAll(s);
+        return output;
     }
 }
