dde8fb5687b4ecfa90fd7bc7624e21c22ca97e3c
DEVANSHU PATIL <152208944+patildevanshu@users.noreply.github.com>
2025-02-08 22:43:03 +0530

    Time: 1 ms (65.25%), Space: 41.6 MB (52.46%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        Arrays.sort(strs);
+        String str1 = strs[0];
+        String str2 = strs[strs.length -1];
+
+        int idx = 0;
+        while(idx < str1.length()){
+            if(str1.charAt(idx) == str2.charAt(idx)){
+                idx++;
+            }else{
+                break;
+            }
+        }
+        return idx==0?"" : str1.substring(0 , idx);
+    }
+}
