7cebd6047bb339a22ab47538812482293b34ce65
DEVANSHU PATIL <152208944+patildevanshu@users.noreply.github.com>
2025-09-03 23:53:24 +0530

    Time: 8 ms (96.89%), Space: 48.6 MB (5.14%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -2,41 +2,39 @@ class Solution {
     public int[][] updateMatrix(int[][] grid) {
         int n = grid.length;
         int m = grid[0].length;
-        
         int[][] ans = new int[n][m];
-        for (int i = 0; i < n; i++) {
-            Arrays.fill(ans[i], Integer.MAX_VALUE);
+
+        // initailize with max value
+        for(int i=0; i<n ; i++){
+            Arrays.fill(ans[i] , Integer.MAX_VALUE -100);
         }
-        
-        Queue<int[]> q = new LinkedList<>();
-        
-        // Step 1: Initialize queue with all cells having 1
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (grid[i][j] == 0) {
+
+        // first pass form top-left to buttom-right
+        for(int i=0; i<n ; i++){
+            for(int j=0; j<m ; j++){
+                if(grid[i][j] == 0){
                     ans[i][j] = 0;
-                    q.add(new int[]{i, j});
+                }else{
+                    if(i>0){
+                        ans[i][j] = Math.min(ans[i][j] , ans[i-1][j] + 1 );
+                    }
+                    if(j>0){
+                        ans[i][j] = Math.min(ans[i][j] , ans[i][j-1] +1);
+                    }
                 }
             }
         }
-        
-        // Step 2: BFS
-        int[][] directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-        
-        while (!q.isEmpty()) {
-            int[] cell = q.poll();
-            int x = cell[0];
-            int y = cell[1];
-            
-            for (int[] d : directions) {
-                int nx = x + d[0];
-                int ny = y + d[1];
-                
-                if (nx >= 0 && nx < n && ny >= 0 && ny < m 
-                    && ans[nx][ny] == Integer.MAX_VALUE) {
-                    
-                    ans[nx][ny] = ans[x][y] + 1;
-                    q.add(new int[]{nx, ny});
+
+        // Second pass , buttom right t0 topleft
+        for(int i=n-1 ; i>=0 ; i--){
+            for(int j=m-1 ; j>=0 ; j--){
+                if(grid[i][j] != 0){
+                    if(i<n-1){
+                        ans[i][j] = Math.min(ans[i][j] , ans[i+1][j] + 1 );
+                    }
+                    if(j<m-1){
+                        ans[i][j] = Math.min(ans[i][j] , ans[i][j+1] +1);
+                    }
                 }
             }
         }
